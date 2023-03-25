81d6d03feca503f031c81899a73517718687f946
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2023-03-25 18:04:22 -0300

    jogo

---
+++ SEMANA_07/02_ATIVIDADES/jogo_ponderada/KinematicBody2D2.gd
@@ -0,0 +1,16 @@
+extends KinematicBody2D
+
+
+func _physics_process(delta):
+	var mov = Vector2.ZERO
+	mov.x = Input.get_action_strength("ui_right") -Input.get_action_strength("ui_left")
+	move_and_slide(mov*700)
+	
+	mov.y = -Input.get_action_strength("ui_up") +Input.get_action_strength("ui_down")
+	move_and_slide(mov*700)
+
+	pass # Replace with function 
+
+func _on_Area2D_body_entered(body):
+	get_tree().change_scene("res://Node2Dtela7.tscn")
+	pass # Replace with function body.
---
+++ SEMANA_07/02_ATIVIDADES/jogo_ponderada/Node2D.tela3.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+
+
+func _on_Button_pressed():
+	get_tree().change_scene("res://Node2D.tela4.tscn")
+	pass # Replace with function body.
